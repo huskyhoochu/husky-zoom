ec49ce0bfa622ab04e161d4601637abbd0ed3da3
huskyhoochu <dfg1499@gmail.com>
2021-10-10 15:08:56 +0900

    firebase 설치 및 환경변수 설정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Router } from '@vaadin/router';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 import './my-app';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 const router = new Router(document.getElementById('outlet'));
 router.setRoutes([{ path: '/', component: 'my-app' }]);

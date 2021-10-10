63cd2e52f0a0f765d9d1e157fd0e13d0946fb7fd
huskyhoochu <dfg1499@gmail.com>
2021-10-10 10:25:25 +0900

    env에 따라 client 참조 위치 변경

--- server/index.ts
+++ server/index.ts
@@ -4,9 +4,10 @@ import compression from 'compression';
 
 const app = express();
 const server = http.createServer(app);
+const isProd = process.env.NODE_ENV === 'production';
 
 app.use(compression())
-app.use(express.static('dist/client'));
+app.use(express.static(isProd ? 'client' : 'dist/client'));
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`server is running http://localhost:${PORT} ..`));
